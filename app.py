cf7f8d33b492ad38c6c39a56c6a4c06b42e3d905
Lee <lee.doolan@gmail.com>
2015-10-31 14:08:10 -0700

    comment out the models.  usinf reflection

--- app.py
+++ app.py
@@ -7,31 +7,37 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+db.reflect()
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<Name %r>' % self.name
+##  class User(db.Model):
+##      id = db.Column(db.Integer, primary_key=True)
+##      first_name = db.Column(db.String(80))
+##      last_name = db.Column(db.String(80))
+##      userid = db.Column(db.String(80))
+##  
+##      def __init__(self, first_name, last_name, userid):
+##          self.first_name = first_name
+##          self.last_name = last_name
+##          self.userid = userid
+##  
+##      def __repr__(self):
+##          return '<User %r>' % self.name
+##  
+##  class Group(db.Model):
+##      id = db.Column(db.Integer, primary_key=True)
+##      self.name = db.Column(db.String(80))
+##  
+##      def __init__(self, first_name, last_name, userid):
+##          self.name = name
+##  
+##      def __repr__(self):
+##          return '<Group %r>' % self.name
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-@app.route('/robots.txt')
-def robots():
-    res = app.make_response('User-agent: *\nAllow: /')
-    res.mimetype = 'text/plain'
-    return res
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
