140b0ecf6333c4a992e0ec597d485a8fcf22c4dc
Lee <lee.doolan@gmail.com>
2015-10-31 23:28:34 -0700

    add /users/<userid>

--- app.py
+++ app.py
@@ -1,42 +1,54 @@
 import os
+import dumper
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, joinedload
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 db.reflect()
 
-##  class User(db.Model):
-##      id = db.Column(db.Integer, primary_key=True)
-##      first_name = db.Column(db.String(80))
-##      last_name = db.Column(db.String(80))
-##      userid = db.Column(db.String(80))
-##  
-##      def __init__(self, first_name, last_name, userid):
-##          self.first_name = first_name
-##          self.last_name = last_name
-##          self.userid = userid
-##  
-##      def __repr__(self):
-##          return '<User %r>' % self.name
-##  
-##  class Group(db.Model):
-##      id = db.Column(db.Integer, primary_key=True)
-##      self.name = db.Column(db.String(80))
-##  
-##      def __init__(self, first_name, last_name, userid):
-##          self.name = name
-##  
-##      def __repr__(self):
-##          return '<Group %r>' % self.name
+class UsersGroups(db.Model):
+    __tablename__ = 'users_groups'
+    group = relationship("Groups", backref="users_assocs", lazy='joined')
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    groups = relationship("UsersGroups", backref="users", lazy='joined')
+
+class Groups(db.Model):
+    __tablename__ = 'groups'
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+@app.route('/users/<uid>', methods=['GET'])
+def users_get(uid):
+    q= Users.query.filter(Users.userid == str(uid).lower())
+    if (q.count() == 0):
+        return not_found()
+
+    usr= q.first()
+    attrs= ('first_name','last_name','userid')
+    retcode= dict((key, str(usr.__dict__.get(key, None))) for key in attrs)
+    retcode['groups'] = [entry.group.name for entry in usr.groups ]
+    return(jsonify(retcode))
+
 
 
 if __name__ == '__main__':
